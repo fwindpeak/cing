499503171610786a44ae764ceeb67bbe29109cde
fwindpeak <fwindpeak@gmail.com>
2021-12-02 09:37:17 +0800

    fix: fix node env error

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # cing
+
 命令行形式发送博客园（cnblogs）闪存
+
+## 安装
+
+```sh
+npm i -g cing
+```
+
+## 使用
+
+### 配置 cookie
+
+- 用浏览器访问[https://ing.cnblogs.com/](https://ing.cnblogs.com/)，登录后按 F12 打开调试工具。
+- 选中`Network`，
+- 刷新页面
+- 随便选一个链接
+- 在`Request Headers`中复制`cookie`
+- 配置`cookie`
+
+```sh
+cing init "<刚才复制的cookie>"
+```
+
+### 发送公开闪存
+
+```sh
+cing 这是公开闪存
+```
+
+### 发送私有闪存
+
+```sh
+cing 这是私有闪存
+```
---
+++ index.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const { program } = require('commander')
+const got = require('got')
+
+program.version('1.0.1')
+
+const CONFIG_FILE_PATH = '/usr/local/etc/cing.json'
+
+POST_URL = 'https://ing.cnblogs.com/ajax/ing/Publish'
+HEADERS = {
+  accept: 'application/json, text/javascript, */*; q=0.01',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'zh-CN,zh;q=0.9',
+  'content-type': 'application/x-www-form-urlencoded',
+  cookie: '',
+  dnt: '1',
+  origin: 'https://ing.cnblogs.com',
+  referer: 'https://ing.cnblogs.com/',
+  'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="6"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-origin',
+  'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
+  'x-requested-with': 'XMLHttpRequest'
+}
+
+CONTENT = {
+  content: '',
+  publicFlag: 1
+}
+
+function readCookie() {
+  try {
+    const data = fs.readFileSync(CONFIG_FILE_PATH)
+    return JSON.parse(data).cookie
+  } catch (error) {
+    return ''
+  }
+}
+
+function writeCookie(cookie) {
+  const data = JSON.stringify({ cookie: cookie })
+  fs.writeFile(CONFIG_FILE_PATH, data, (err) => {
+    if (err) {
+      console.log('write error', err.message)
+    } else {
+      console.log('cookie write success')
+    }
+  })
+}
+
+program
+  .command('init <cookie>')
+  .description('set a cookie')
+  .action((cookie) => {
+    writeCookie(cookie)
+  })
+
+program
+  .arguments('<content>')
+  .option('-p, --private', 'private post')
+  .option('-c, --cookie <cookie>', 'set cookie string')
+  .action((content, options, command) => {
+    CONTENT.publicFlag = options.private ? 0 : 1
+    CONTENT.content = content
+    let cookie = readCookie()
+    if (options.cookie) {
+      cookie = options.cookie
+      writeCookie(cookie)
+    }
+    HEADERS.cookie = cookie
+    if (cookie) {
+      publish()
+    } else {
+      console.log('cookie is empty')
+    }
+  })
+  .parse()
+
+function publish() {
+  got
+    .post(POST_URL, {
+      form: CONTENT,
+      responseType: 'json',
+      headers: HEADERS,
+      https: {
+        rejectUnauthorized: false
+      }
+    })
+    .then((res) => {
+      console.log(res.body)
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
